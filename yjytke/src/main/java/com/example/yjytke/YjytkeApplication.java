4bc02517606da5ad71c3fd6fcdf3c01c2f23fa49
wuynje <wuynje@163.com>
2018-08-09 11:24:00 +0800

    first commit

---
+++ yjytke/src/main/java/com/example/yjytke/YjytkeApplication.java
@@ -0,0 +1,12 @@
+package com.example.yjytke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjytkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YjytkeApplication.class, args);
+	}
+}
